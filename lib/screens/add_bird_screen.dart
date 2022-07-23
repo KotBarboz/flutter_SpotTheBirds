483f8bc5cab8ee773d0308da366c2916d55f76c4
Konstantin <kotplansh@gmail.com>
2022-07-23 17:47:46 +0300

    add model

--- lib/screens/add_bird_screen.dart
+++ lib/screens/add_bird_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:spot_the_bird/models/bird_post_model.dart';
 
 class AddBirdScreen extends StatefulWidget {
   final LatLng latLng;
@@ -14,39 +15,66 @@ class AddBirdScreen extends StatefulWidget {
 }
 
 class _AddBirdScreenState extends State<AddBirdScreen> {
+  String? name;
+  String? description;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Add bird"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width / 2,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  image: FileImage(widget.image),
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width / 1.4,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: FileImage(widget.image),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            // Text("${widget.latLng.latitude} ${widget.latLng.longitude}"),
-            // ElevatedButton(
-            //     onPressed: () => Navigator.of(context).pop(),
-            //     child: const Text("Back"))
-            const TextField(),
-            const TextField(),
-          ],
+              // Text("${widget.latLng.latitude} ${widget.latLng.longitude}"),
+              // ElevatedButton(
+              //     onPressed: () => Navigator.of(context).pop(),
+              //     child: const Text("Back"))
+              TextField(
+                onChanged: (value) {
+                  name = value;
+                },
+              ),
+              TextField(
+                onChanged: (value) {
+                  description = value;
+                },
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           //
+
+          if (description != null && name != null) {
+            if (description != "" && name != "") {
+              final BirdModel birdModel = BirdModel(
+                  image: widget.image,
+                  latitude: widget.latLng.latitude,
+                  longitude: widget.latLng.longitude,
+                  birdDescription: description!,
+                  birdName: name!);
+            }
+          }
+
+          Navigator.of(context).pop();
         },
         child: const Icon(
           Icons.check,
